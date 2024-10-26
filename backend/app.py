9b5915d9f6b2d909275eb1d8875c67ed76f01f8e
Elijah <eolokun1806@gmail.com>
2024-10-26 15:07:23 +0100

    added buy/sell endpoints

--- backend/app.py
+++ backend/app.py
@@ -52,7 +52,10 @@ def signup():
             "role": role,
             "balance": 0.0,
             "date_created": datetime.now(), 
-            "date_updated": datetime.now(), 
+            "date_updated": datetime.now(),
+            "trades": [],
+            "transactions": [],
+            "stocks": {}
         }
         result = mongo.db.users.insert_one(user_data)        
 
@@ -101,7 +104,10 @@ def get_user_by_id(user_id):
             "id": str(user["_id"]),
             "email": user.get("email"),
             "balance": user.get("balance", 0.0),
-            "role": user.get("role", "USER")
+            "role": user.get("role", "USER"),
+            "transactions": [str(txn) for txn in user.get("transactions", [])],
+            "trades": [str(trade) for trade in user.get("trades", [])],
+            "stocks": user.get("stocks", {})
         }
         return jsonify(user_data), 200
 
@@ -123,15 +129,18 @@ def withdraw(user_id):
             return jsonify({'error': 'Insufficient funds'}), 400
 
         updated_balance = current_balance - amount
+        
         mongo.db.users.update_one({"_id": ObjectId(user_id)}, {"$set": {"balance": updated_balance}})
+        
         transaction_data = {
             "user_id": user_id,
             "type": "WITHDRAWAL",
             "amount": amount,
             "date_created": datetime.now()
         }
-        mongo.db.transactions.insert_one(transaction_data)
-        
+        transaction_id = mongo.db.transactions.insert_one(transaction_data).inserted_id
+        mongo.db.users.update_one({"_id": ObjectId(user_id)}, {"$push": {"transactions": transaction_id}})
+    
         return jsonify({'message': 'Withdrawal successful!'}), 200
     except Exception as e:
         return generic_error_response(e)
@@ -156,7 +165,8 @@ def deposit(user_id):
             "amount": amount,
             "date_created": datetime.now()
         }
-        mongo.db.transactions.insert_one(transaction_data)
+        transaction_id = mongo.db.transactions.insert_one(transaction_data).inserted_id
+        mongo.db.users.update_one({"_id": ObjectId(user_id)}, {"$push": {"transactions": transaction_id}})
         
         return jsonify({'message': 'Deposit successful!'}), 200
     except Exception as e:
@@ -241,6 +251,112 @@ def search_stocks():
 
     stock_list = [{"id": str(stock["_id"]), "symbol": stock["symbol"], "name": stock["name"]} for stock in results]
     return jsonify(stock_list), 200
+
+@app.route('/api/users/<user_id>/buy', methods=['POST'])
+def buy_stock(user_id):
+    data = request.get_json()
+    stock_id = data.get('stock_id')
+    quantity = data.get('quantity', 0)
+    
+    try:
+        user = mongo.db.users.find_one({"_id": ObjectId(user_id)})
+        stock = mongo.db.stocks.find_one({"_id": ObjectId(stock_id)})
+        
+        if not user:
+            return jsonify({'error': 'User not found'}), 404
+        if not stock:
+            return jsonify({'error': 'Stock not found'}), 404
+
+        stock_price = stock.get("price", 0.0)
+        total_cost = stock_price * quantity
+        user_balance = user.get("balance", 0.0)
+
+        if user_balance < total_cost:
+            return jsonify({'error': 'Insufficient funds'}), 400
+
+        updated_balance = user_balance - total_cost
+        updated_quantity = user["stocks"].get(stock_id, 0.0) + quantity
+        mongo.db.users.update_one({"_id": ObjectId(user_id)}, {"$set": {"balance": updated_balance, f"stocks.{stock_id}": updated_quantity}})
+        
+        trade_data = {
+            "user_id": user_id,
+            "stock_id": stock_id,
+            "type": "BUY",
+            "quantity": quantity,
+            "amount": total_cost,
+            "date_created": datetime.now()
+        }
+        trade_id = mongo.db.trades.insert_one(trade_data).inserted_id
+        mongo.db.users.update_one({"_id": ObjectId(user_id)}, {"$push": {"trades": trade_id}})
+
+        return jsonify({
+            'message': 'Sale successful!',
+            'quantity': quantity,
+            'symbol': stock.get("symbol"),
+            'price': stock_price,
+            'final_balance': updated_balance
+        }), 200
+    except Exception as e:
+        return generic_error_response(e)
+    
+@app.route('/api/users/<user_id>/sell', methods=['POST'])
+def sell_stock(user_id):
+    data = request.get_json()
+    stock_id = data.get('stock_id')
+    quantity = data.get('quantity', 0)
+    
+    try:
+        user = mongo.db.users.find_one({"_id": ObjectId(user_id)})
+        stock = mongo.db.stocks.find_one({"_id": ObjectId(stock_id)})
+        
+        if not user:
+            return jsonify({'error': 'User not found'}), 404
+        if not stock:
+            return jsonify({'error': 'Stock not found'}), 404
+        
+        quantity_owned = user["stocks"].get(stock_id, 0.0)
+        if quantity_owned < quantity:
+            return jsonify({'error': 'Insufficient quantity'}), 400
+
+        # TODO: Refactor into helper function
+        stock_data = get_stock_data(stock['symbol'])
+        updated_stock = {
+                "price": stock_data['price'],  
+                "date_updated": datetime.now(),
+            }
+
+        mongo.db.stocks.update_one({"_id": ObjectId(stock_id)}, {"$set": updated_stock})
+        stock.update(updated_stock)
+
+        stock_price = stock.get("price", 0.0)
+        total_cost = stock_price * quantity
+        user_balance = user.get("balance", 0.0)
+
+        updated_balance = user_balance + total_cost
+        updated_quantity = user["stocks"].get(stock_id, 0.0) - quantity 
+        mongo.db.users.update_one({"_id": ObjectId(user_id)}, {"$set": {"balance": updated_balance, f"stocks.{stock_id}": updated_quantity}})
+        
+        trade_data = {
+            "user_id": user_id,
+            "stock_id": stock_id,
+            "type": "SELL",
+            "quantity": quantity,
+            "amount": total_cost,
+            "date_created": datetime.now()
+        }
+        trade_id = mongo.db.trades.insert_one(trade_data).inserted_id
+        mongo.db.users.update_one({"_id": ObjectId(user_id)}, {"$push": {"trades": trade_id}})
+        
+        return jsonify({
+            'message': 'Sale successful!',
+            'quantity': quantity,
+            'symbol': stock.get("symbol"),
+            'price': stock_price,
+            'final_balance': updated_balance
+        }), 200
+        
+    except Exception as e:
+        return generic_error_response(e)
 
 if __name__ == '__main__':
     app.run(debug=True)
